3823b43a204072e5326d2f759b0f29c17c855d4a
Dominic Simpson <52511353+DominicSimpson@users.noreply.github.com>
2022-07-30 12:32:33 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,30 @@
+// Select all slides
+const prevSlider = document.getElementById("prevSlider");
+const nextSlider = document.getElementById("nextSlider");
+const dotOne = document.getElementById("dotOne");
+const dotTwo = document.getElementById("dotTwo");
+const dotThree = document.getElementById("dotThree");
+prevSlider.addEventListener("click", function (){
+  showSlides(slideIndex += -1);
+});
+nextSlider.addEventListener("click", function (){
+  showSlides(slideIndex += 1);
+});
+dotOne.addEventListener("click", function (){
+  showSlides(slideIndex = 1);
+});
+dotTwo.addEventListener("click", function (){
+  showSlides(slideIndex = 2);
+});
+dotThree.addEventListener("click", function (){
+  showSlides(slideIndex = 3);
+});
 let slideIndex = 1;
 showSlides(slideIndex);
-
-// Next/previous controls
-
-
-document.getElementById('myBtn1').addEventListener('click', plusSlides)
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-  sliderButton.className = "prev";
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   let i;
-  let slides = document.querySelectorAll(".mySlides");
-  let dots = document.querySelectorAll(".dot");
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -31,14 +36,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-
-
-
-
-
-
-
-
-
-// Submit button triggers function above
